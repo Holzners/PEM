90683491704188bfe675a0fcf5d0bb222c2ff83d
olliongit <O.Duerr@campus.lmu.de>
2015-06-18 17:15:04 +0200

    Implementing the Slide Tab Layout still some bugs

--- app/src/main/java/com/team3/pem/pem/MainActivity.java
+++ app/src/main/java/com/team3/pem/pem/MainActivity.java
@@ -3,13 +3,16 @@ package com.team3.pem.pem;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.support.v7.widget.Toolbar;
 import com.roomorama.caldroid.CaldroidFragment;
 import com.team3.pem.pem.view.CalendarFragment;
+import com.team3.pem.pem.view.SlidingTabLayout;
+import com.team3.pem.pem.view.ViewPagerAdapter;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -23,11 +26,49 @@ public class MainActivity extends ActionBarActivity {
 
     CalendarFragment caldroidFragment;
    // SwitchFragment switchFragment;
+    // Declaring Your View and Variables
+
+    Toolbar toolbar;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"Home","Events"};
+    int Numboftabs =2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Creating The Toolbar and setting it as the Toolbar for the activity
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
+
+
         daysToModify = new HashMap<>();
 
         initMonthFragment();
@@ -67,7 +108,7 @@ public class MainActivity extends ActionBarActivity {
         caldroidFragment.setArguments(args);
         caldroidFragment.refreshView();
         android.support.v4.app.FragmentTransaction t = getSupportFragmentManager().beginTransaction();
-        t.add(R.id.contentPanel, caldroidFragment);
+        t.add(R.id.calender, caldroidFragment);
         t.commit();
     }
 
