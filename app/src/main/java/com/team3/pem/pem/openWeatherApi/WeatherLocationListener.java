d146b213a66c4e770298f7cbd499d62078ae944f
holzners <von-mir1@gmx.de>
2015-06-29 13:57:31 +0200

    WeatherListeenr

---
+++ app/src/main/java/com/team3/pem/pem/openWeatherApi/WeatherLocationListener.java
@@ -0,0 +1,30 @@
+package com.team3.pem.pem.openWeatherApi;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by Stephan on 28.06.15.
+ */
+public class WeatherLocationListener implements LocationListener {
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
