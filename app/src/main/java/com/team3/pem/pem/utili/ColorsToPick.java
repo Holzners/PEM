ea5f7583e09ad81b4e498296b6329d10a7f6aad6
susahne <susahne.eff@gmail.com>
2015-07-04 19:59:08 +0200

    .

--- app/src/main/java/com/team3/pem/pem/utili/ColorsToPick.java
+++ app/src/main/java/com/team3/pem/pem/utili/ColorsToPick.java
@@ -7,11 +7,11 @@ import com.team3.pem.pem.R;
  */
 public enum ColorsToPick {
 
-    GREEN(R.color.green1 , R.color.green2 ,R.color.green3 ,R.color.green4 ,R.color.green5),
-    RED(R.color.red1 , R.color.red2 ,R.color.red3 ,R.color.red4 ,R.color.red5),
-    BLUE(R.color.blue1 , R.color.blue2 ,R.color.blue3 ,R.color.blue4 ,R.color.blue5),
-    YELLOW(R.color.yellow1 , R.color.yellow2 ,R.color.yellow3 ,R.color.yellow4 ,R.color.yellow5),
     VIOLETTE(R.color.violette1 , R.color.violette2 ,R.color.violette3 ,R.color.violette4 ,R.color.violette5),
+    BLUE(R.color.blue1 , R.color.blue2 ,R.color.blue3 ,R.color.blue4 ,R.color.blue5),
+    GREEN(R.color.green1 , R.color.green2 ,R.color.green3 ,R.color.green4 ,R.color.green5),
+    YELLOW(R.color.yellow1 , R.color.yellow2 ,R.color.yellow3 ,R.color.yellow4 ,R.color.yellow5),
+    RED(R.color.red1 , R.color.red2 ,R.color.red3 ,R.color.red4 ,R.color.red5),
     BROWN(R.color.brown1 , R.color.brown2 ,R.color.brown3 ,R.color.brown4 ,R.color.brown5);
 
     private int color1, color2 , color3 , color4, color5;
