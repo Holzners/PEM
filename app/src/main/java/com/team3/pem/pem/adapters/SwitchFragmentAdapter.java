2d4ebb35545c1b8bdbb072359302499adc9295f0
susahne <susahne.eff@gmail.com>
2015-07-04 17:26:24 +0200

    Switchfarben

--- app/src/main/java/com/team3/pem/pem/adapters/SwitchFragmentAdapter.java
+++ app/src/main/java/com/team3/pem/pem/adapters/SwitchFragmentAdapter.java
@@ -2,6 +2,7 @@ package com.team3.pem.pem.adapters;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -47,17 +48,11 @@ public class SwitchFragmentAdapter extends ArrayAdapter<String> {
         Switch mSwitch = (Switch) newView.findViewById(R.id.switch1);
         mSwitch.setText(getItem(position));
         mSwitch.setChecked(mDBHelper.getFactorEnabledMap().get(getItem(position)));
-
-        mSwitch.setOnCheckedChangeListener(new SwitchOnCheckedChangedListener(getItem(position)));
-        mSwitch.setText(getItem(position));
-        mSwitch.setChecked(mDBHelper.getFactorEnabledMap().get(getItem(position)));
         mSwitch.setOnCheckedChangeListener(new SwitchOnCheckedChangedListener(getItem(position)));
 
-        String string = mDBHelper.getFactorColorMap().get(getItem(position));
-        int color = RatingToColorHelper.ratingToColor(string, 3);
-        mSwitch.getThumbDrawable().setTint(color);
-//        mSwitch.getTrackDrawable().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
-//        mSwitch.setThumbDrawable(context.getDrawable(R.drawable.abc_btn_radio_material));
+        String s = mSwitch.getText().toString();
+        int color = context.getResources().getColor(RatingToColorHelper.ratingToColor(s, 3));
+        mSwitch.getThumbDrawable().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
 
         mSwitch.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -80,7 +75,7 @@ public class SwitchFragmentAdapter extends ArrayAdapter<String> {
 
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            context.switchSymptom(isChecked , symptom);
+            context.switchSymptom(isChecked, symptom);
         }
     }
 }
--- app/src/main/java/com/team3/pem/pem/utili/RatingToColorHelper.java
+++ app/src/main/java/com/team3/pem/pem/utili/RatingToColorHelper.java
@@ -15,17 +15,19 @@ public class RatingToColorHelper {
 
         HashMap<String, String> factorWidthColor = mdHelper.getFactorsFromDatabase();
         if(factor == null || rating == 0) return R.color.white;
+        String s = factorWidthColor.get(factor);
+        ColorsToPick c = ColorsToPick.getColorByString(s);
         switch (rating) {
             case 1:
-                return ColorsToPick.getColorByString(factorWidthColor.get(factor)).getColor1();
+                return c.getColor1();
             case 2:
-                return ColorsToPick.getColorByString(factorWidthColor.get(factor)).getColor2();
+                return c.getColor2();
             case 3:
-                return ColorsToPick.getColorByString(factorWidthColor.get(factor)).getColor3();
+                return c.getColor3();
             case 4:
-                return ColorsToPick.getColorByString(factorWidthColor.get(factor)).getColor4();
+                return c.getColor4();
             case 5:
-                return ColorsToPick.getColorByString(factorWidthColor.get(factor)).getColor5();
+                return c.getColor5();
             default:
                 return R.color.transparent;
         }
