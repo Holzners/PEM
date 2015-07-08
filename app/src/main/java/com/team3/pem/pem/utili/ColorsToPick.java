c916f5dba3fa68eb78e52629fcebe976020a7e60
olliongit <O.Duerr@campus.lmu.de>
2015-07-08 14:57:46 +0200

    Icons

--- app/src/main/java/com/team3/pem/pem/utili/ColorsToPick.java
+++ app/src/main/java/com/team3/pem/pem/utili/ColorsToPick.java
@@ -7,12 +7,13 @@ import com.team3.pem.pem.R;
  */
 public enum ColorsToPick {
 
-    VIOLETTE(R.color.violette1 , R.color.violette2 ,R.color.violette3 ,R.color.violette4 ,R.color.violette5),
     BLUE(R.color.blue1 , R.color.blue2 ,R.color.blue3 ,R.color.blue4 ,R.color.blue5),
+    CYAN(R.color.cyan1 , R.color.cyan2 ,R.color.cyan3 ,R.color.cyan4 ,R.color.cyan5),
     GREEN(R.color.green1 , R.color.green2 ,R.color.green3 ,R.color.green4 ,R.color.green5),
     YELLOW(R.color.yellow1 , R.color.yellow2 ,R.color.yellow3 ,R.color.yellow4 ,R.color.yellow5),
     RED(R.color.red1 , R.color.red2 ,R.color.red3 ,R.color.red4 ,R.color.red5),
-    BROWN(R.color.brown1 , R.color.brown2 ,R.color.brown3 ,R.color.brown4 ,R.color.brown5);
+    VIOLETTE(R.color.violette1 , R.color.violette2 ,R.color.violette3 ,R.color.violette4 ,R.color.violette5);
+
 
     private int color1, color2 , color3 , color4, color5;
 
@@ -55,7 +56,7 @@ public enum ColorsToPick {
             else if (color.equalsIgnoreCase(RED.name())) return RED;
             else if (color.equalsIgnoreCase(BLUE.name())) return BLUE;
             else if (color.equalsIgnoreCase(YELLOW.name())) return YELLOW;
-            else if (color.equalsIgnoreCase(BROWN.name())) return BROWN;
+            else if (color.equalsIgnoreCase(CYAN.name())) return CYAN;
         }
         return VIOLETTE;
     }
